9af350cb1423db153577802b10ef4fbef54dad9e
Sagar Lal <sagar.lal@columbia.edu>
2019-04-02 00:27:23 -0400

    Added comments to scanner and modified tests such that comments are written with /# and #/

---
+++ got/_build/Gotparser.mli
@@ -0,0 +1,47 @@
+type token =
+  | SEMI
+  | LPAREN
+  | RPAREN
+  | LBRACE
+  | RBRACE
+  | COMMA
+  | PLUS
+  | MINUS
+  | TIMES
+  | DIVIDE
+  | MOD
+  | EXP
+  | ASSIGN
+  | NOT
+  | EQ
+  | NEQ
+  | LT
+  | LEQ
+  | GT
+  | GEQ
+  | AND
+  | OR
+  | RETURN
+  | IF
+  | ELSE
+  | ELIF
+  | FOR
+  | WHILE
+  | INT
+  | BOOL
+  | FLOAT
+  | VOID
+  | STRING
+  | ACCESS
+  | NEW
+  | DEF
+  | AT_STEP
+  | SELF
+  | LITERAL of (int)
+  | BLIT of (bool)
+  | ID of (string)
+  | FLIT of (string)
+  | EOF
+
+val program :
+  (Lexing.lexbuf  -> token) -> Lexing.lexbuf -> Ast.program
